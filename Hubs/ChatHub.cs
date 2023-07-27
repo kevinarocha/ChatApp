e71c4d0124992bc6ce4c17bb0b9034cacfb2c6dc
Kevin Rocha <42311410+kevinarocha@users.noreply.github.com>
2023-07-26 19:31:33 -0700

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatAppMVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SendMessage(string user, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", user, message);
+            
+
+        }
+    }
+}
